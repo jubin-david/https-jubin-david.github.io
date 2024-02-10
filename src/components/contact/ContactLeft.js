843a6d0efd191403041c87518f50a54e5e70fcc3
jubindavid <jubindavid390@gmail.com>
2024-02-10 08:45:48 +0530

    Added redirections in contact section

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { FaInstagram, FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { contactImg } from "../../assets/index";
 
+const redirectToInstagram = () => {
+  window.open('https://www.instagram.com/jubin_david', '_blank'); // Replace 'your-username' with your Instagram username or the desired URL
+};
+
+const redirectToLinkedIn = () => {
+  window.open('https://www.linkedin.com/in/jubin-david', '_blank'); // Replace 'your-username' with your LinkedIn username or the desired URL
+};
+
+const redirectToGitHub = () => {
+  window.open('https://github.com/jubin-david/', '_blank');
+}
 const ContactLeft = () => {
   return (
     <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
@@ -25,13 +36,13 @@ const ContactLeft = () => {
       <div className="flex flex-col gap-4">
         <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
-          <span className="bannerIcon">
+          <span className="bannerIcon" onClick={redirectToInstagram}>
             <FaInstagram />
           </span>
-          <span className="bannerIcon">
+          <span className="bannerIcon" onClick={redirectToGitHub}>
             <FaGithub />
           </span>
-          <span className="bannerIcon">
+          <span className="bannerIcon" onClick={redirectToLinkedIn}>
             <FaLinkedinIn />
           </span>
         </div>
